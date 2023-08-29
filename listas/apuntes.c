39246a77de06706d0ae4f0a86bf4e2595fadb7e6
Julisan <80128364+Julian986@users.noreply.github.com>
2023-08-29 20:46:44 -0300

    Update apuntes.c

--- listas/apuntes.c
+++ listas/apuntes.c
@@ -1,4 +1,8 @@
-clase 29-8
+//clase 29-8
 
-Los nodos se comunican por su direccion de memoria
+//Los nodos se comunican por su direccion de memoria
+typedef struct _nodo{
+    cli dato;
+    struct _nodo*sig;
+}nodo;
 
